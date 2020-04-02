1072f1e2ba641b01ac08a9c5cde71a6a5fb88388
edheadonfire <eduardo.preciado.394@my.csun.edu>
2020-04-01 18:26:33 -0700

    combined files into grammar.py

--- grammar.py
+++ grammar.py
@@ -21,3 +21,74 @@ class String:
 class Variable: 
     def __init__(self):
         self.value = ["Pets", "Pies"]
+
+
+# expression
+class e:
+    def __init__():
+        self.value =  [
+                       n(), # integer
+                       b(), # boolean
+                       str(), # string
+                       "null", # Null
+                       x(),  # variable
+                       x() + "=" + e(),
+                       x() + cbop() + "=" + e(),
+                       e() + e() + "=" e(),
+                       e() + "." + e() op() + "=" + e(),
+                       e()  + "?" e() +":" + e(),
+                       e() + "." + e(),
+                       "new" + " "+ e() + "(" +")",
+                       "new" + " "+ e() + "(" + e() + ")",
+                       e() + "("  + ")",
+                       e() + "(" + e() + ")",
+                       "function" + "[" +x() + "]" + "("+ ")" + s() ,
+                       "function" + "[" +x() + "]" + "(" + e() +")" + s() ,
+                       e() + bop() + e() ,
+                       unOp() +  e() ,
+                       "{" + "}" ,
+                       "{" + "<"str() +"," + e()+ ">"+ "}" ,
+                       "["+ "]",
+                       "["+ e()+ "]",
+                       "this",
+                       "delete" + e(),
+                       "++" + x(),
+                       "++" + e()+ "." + e(),
+                       x() + "++" ,
+                       e()+ "." + e() + "++" ,
+                       "--"+ x(),
+                       "--" + e()+"."+e(),
+                       x()+"--" ,
+                       e()+"."+e()+"--" ,
+                       "eval" + s(),
+                    ]
+    def eval():
+
+
+class s:
+    def __init__(self):
+        self.value = [
+            e(),
+            "'\'" + "<{" + s() + "}>",
+            "while" + "(" + e() + "){" + s() + "}",
+            "do{" + s() + "}" + "while(" + e() + ")",
+            "for(" + x() + " in " + s() + ")",
+            "for(" + e() + "." + e() + " in " + s() + ")",
+            "for(" + s() + ";" + e() + ";" + s() + ";" + ")",
+            "var" + x() + "=" + e() + "[," + x() + "=" + e() + "...]",
+            "function" + x() + "'\'" + "<{" + x() + "}>" + s(),
+            "if(" + e() + ")" + s() + " [" + s() + "]",
+            "try([" + x() + s() + "] [" + s() + "])",
+            "throw(" + e() + ")",
+            "lbl:" + s(),
+            "break [" + "lbl" + "]",
+            "continue [" + "lbl" + "]",
+            "with " + e() + s(),
+            "return [" + e() + "]",
+            "switch" + e() + "'\'" + "<{" + sw() + "}>",  # or s() + w() ?
+        ]
+
+    def eval(self):
+        return 'a statement'
+
+
