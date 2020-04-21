aa35df20985e18422ed7c1e3c54b033b650e1778
edheadonfire <eduardo.preciado.394@my.csun.edu>
2020-04-20 23:38:53 -0700

    trying to fix bugs with next generation

--- edsGrammar.py
+++ edsGrammar.py
@@ -3,92 +3,76 @@ import inspect
 
 
 class integer:
+    def __init__(self):
+        self.value = [1, 2]
+
     def gen(self, bound):
-        if bound <= 0:
-            yield
-        else:
-            self.value = [1, 2]
-            for each in self.value:
-                yield each
+        for each in self.value:
+            yield each
 
 
 class varName:
+    def __init__(self):
+        self.value = ["PETS", "PIES"]
+
     def gen(self, bound):
-        if bound <= 0:
-            yield
-        else:
-            self.value = ["PETS", "PIES"]
-            for each in self.value:
-                yield each
+        for each in self.value:
+            yield each
 
 
 # eds implementation have class variables represent the inner nodes 4/6/2020
 # take a look please ?
-# class parameterAssignment:
-#     def __init__(self, e1,e2,e3):
-#         self.e1 = e1
-#         self.e2 = e2
-#         self.e3 = e3
-#
-#     def __str__(self):
-#         return self.e1 + "." + self.e2 + " = " + self.e3
-#
-#     def gen(self, bound):
-# technically you should never call this if you have bound <= 0 but its ok to check but maybe we should throw an error
-# instead
-#         if bound <= 0:
-#             yield
-#         else:
-#             for e1 in expression().gen(bound-1):
-#                 for e2 in expression().gen(bound-1):
-#                     for e3 in expression().gen(bound-1):
-#                         yield parameterAssignment(e1, e2, e3)
-
-
-# ed similar to group coding
 class parameterAssignment:
-    def gen(self, bound):
-        if bound <= 0:
-            yield
-        else:
-            for e1 in expression().gen(bound - 1):
-                for e2 in expression().gen(bound - 1):
-                    for e3 in expression().gen(bound - 1):
-                        yield "{}.{} = {}".format(e1, e2, e3)
+    def __init__(self, e1, e2, e3):
+        self.e1 = e1
+        self.e2 = e2
+        self.e3 = e3
 
+    def __str__(self):
+        return self.e1 + "." + self.e2 + " = " + self.e3
 
-class assignment:
     def gen(self, bound):
-        if bound <= 0:
-            yield
-        else:
-            for exp in expression().gen(bound - 1):
-                for var in varName().gen(bound - 1):
-                    yield "{} = {}".format(var, exp)
+        for e1 in expression().gen(bound - 1):
+            for e2 in expression().gen(bound - 1):
+                for e3 in expression().gen(bound - 1):
+                    yield parameterAssignment(e1, e2, e3)
 
 
 # eds implementation of exp
 # only produce a leaf node if the bound is 0
 # removes the need to check if it is a class or not.
+
+def test():
+    for item in expression().gen(1):
+        print(item)
+
+
+class expression:
+    def __init__(self):
+        self.value = [parameterAssignment]
+
+    def gen(self, bound):
+        if bound <= 0:
+            self.value = [integer(), varName()]
+            for each in self.value:
+                yield next(each.gen(0))
+        else:
+            for each in self.value:
+                nextval = bound - 1
+                yield each.gen(nextval)
+
+
+
+
+
 # class expression:
 #     def gen(self, bound):
 #         if bound <= 0:
-#             self.value = [n(), b(), string(), null(), varName()]
-#             for each in self.value:
-#                 yield each.gen()
+#             yield
 #         else:
 #             self.value = [assignment(), parameterAssignment()]
 #             for each in self.value:
-#                 yield each.gen(bound-1)
-
-class expression:
-    def gen(self, bound):
-        if bound <= 0:
-            yield
-        else:
-            self.value = [assignment(), parameterAssignment()]
-            for each in self.value:
-                yield each.gen(bound - 1)
+#                 yield each.gen(bound - 1)
 
 
 class true_false:
@@ -147,3 +131,24 @@ class bool_not:
             yield None
             for component in true_false().gen(bound - 1):
                 yield component
+
+# ed similar to group coding
+# class parameterAssignment:
+#     def gen(self, bound):
+#         if bound <= 0:
+#             yield
+#         else:
+#             for e1 in expression().gen(bound - 1):
+#                 for e2 in expression().gen(bound - 1):
+#                     for e3 in expression().gen(bound - 1):
+#                         yield "{}.{} = {}".format(e1, e2, e3)
+
+
+# class assignment:
+#     def gen(self, bound):
+#         if bound <= 0:
+#             yield
+#         else:
+#             for exp in expression().gen(bound - 1):
+#                 for var in varName().gen(bound - 1):
+#                     yield "{} = {}".format(var, exp)
