5a236477596030dc07c7283b63529ed4042dc81b
Stephanie <Stephanie.contreras.477@my.csun.edu>
2020-05-04 17:01:18 -0700

    made some more changes

--- grammar.py
+++ grammar.py
@@ -25,13 +25,21 @@ class varName:
         for each in self.value:
             yield each
 
+class string:
+    def __init__(self):
+        self.value =["A", "B"]
+
+    def gen(self):
+        for each in self.value:
+            yield each
+
 class expression:
     def __init__(self):
-        self.value = [expressiondotexpression()]
+        self.value = [ExpAssignment(), Exp1DotExp2EqualsExp(), Exp1DotExp2()]
 
     def gen(self, bound):
         if bound <= 0:
-            self.value = [integer(), varName()]
+            self.value = [integer(), varName(), boolean(), string(), None]
             for each in self.value:
                 for item in each.gen(0):
                     yield item
@@ -40,7 +48,34 @@ class expression:
                 for item in each.gen(bound -1):
                     yield item
 
-class expressiondotexpression:
+class ExpAssignment:
+    def __init__(self, v1, e1):
+            self.e1 = e1
+            self.v1 = v1
+
+    def __str__(self):
+        return  str(self.v1)+ "=" + str(self.e1)
+
+    def gen(self, bound):
+        for v1 in varName().gen(bound -1):
+            for e1 in expression().gen(bound -1):
+                 yield ExpAssignment(e1, v1)
+
+class Exp1DotExp2EqualsExp:
+    def __init__(self, e1, e2, e3):
+            self.e1 = e1
+            self.e2 = e2
+            self.e3 = e3
+    def __str__(self):
+        return str(self.e1) + "." + str(self.e2) + "=" + str(self.e3)
+
+    def gen(self, bound):
+        for e1 in expression().gen(bound -1):
+            for e2 in expression().gen(bound-1):
+                for e3 in expression().gen(bound-1):
+                    yield Exp1DotExp2EqualsExp(e1, e2, e3)
+
+class Exp1DotExp2:
     def __init__(self, e1, e2):
             self.e1 = e1
             self.e2 = e2
@@ -51,7 +86,7 @@ class expressiondotexpression:
     def gen(self, bound):
         for e1 in expression().gen(bound -1):
             for e2 in expression().gen(bound-1):
-                yield expressiondotexpression(e1, e2)
+                yield Exp1DotExp2(e1, e2)
 
 class statement:
     def gen(self, bound):
@@ -72,7 +107,7 @@ class ifStatement:
         self.stmt2 = stmt2
 
     def __str__(self):
-        return str("if(" +e1 +") " +stmt1 + " " + stmt2)
+        return "if(" +str(self.e1) +") " +str(self.stmt1) + " " + str(self.stmt2)
 
     def gen(self,bound):
         for e1 in expression().gen(bound -1):
@@ -81,12 +116,12 @@ class ifStatement:
                     yield ifStatement(e1, stmt1, stmt2)
 
 class whileStatement:
-     def __init__(self, e1, stmt1):
+    def __init__(self, e1, stmt1):
         self.e1 = e1
         self.stmt1 = stmt1
 
     def __str__(self):
-        return str("while " +e1 +" " +stmt1 +)
+        return "while " +str(self.e1) +" " +str(self.stmt1)
 
     def gen(self,bound):
         for e1 in expression().gen(bound -1):
